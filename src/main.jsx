9e748bbbb6a15c1fb0feb503b25d22f7434d5954
Kelvin-Ben <kevoben14@gmail.com>
2023-06-25 20:49:21 +0300

    Add sematic ui and modified a few code

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App.jsx";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from "./reducers/index.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={createStore(reducers)}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
