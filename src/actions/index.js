f3845e52749043b932f1b07e08b289cef53ab4b6
Kelvin-Ben <kevoben14@gmail.com>
2023-06-26 22:54:16 +0300

    Making a request from an action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
-export const fetchPost = () => {
+import jsonPlaceholder from "../apis/jsonPlaceholder"
+export const fetchPost = async () => {
+  const response = await jsonPlaceholder.get('/posts')
   return {
-    type: "FETCH_POST"
+    type: "FETCH_POST",
+    payload: response
   }
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Connect, connect } from "react-redux";
+import { connect } from "react-redux";
 import { fetchPost } from "../actions";
 
 class PostList extends Component {
